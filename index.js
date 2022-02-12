249d4da49d457780ad850baef0312b3c9a726ab8
Tanisha-creator <tanishakindo@gmail.com>
2022-02-12 15:58:52 +0530

    Hamberger menu

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = () =>{
+    let x =  document.getElementById("links");
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else
+    x.style.display = "none"
+    };
